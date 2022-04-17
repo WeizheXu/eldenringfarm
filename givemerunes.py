74488aee78a950f488071b4da4e768e866c793b2
WeizheXu <wex013@eng.ucsd.edu>
2022-04-17 10:01:45 -0700

    Initial commit

---
+++ givemerunes.py
@@ -0,0 +1,86 @@
+#import pyautogui
+import pynput
+from pynput.keyboard import Key, Controller
+from pynput.mouse import Button
+from pynput.mouse import Controller as MouseController
+import time
+import numpy as np
+
+def smoothMove(mouse, x, y, duration, interval=0.01):
+    
+    action = np.arange(0, duration, interval)
+    toMoveX = x/len(action)
+    toMoveY = y/len(action)
+
+    for i in action:
+        mouse.move(toMoveX, toMoveY)
+        time.sleep(interval)
+
+def initialPath(keyboard):
+    with keyboard.pressed('w'):
+        time.sleep(3.0)
+    with keyboard.pressed('a'):
+        time.sleep(0.8)
+    with keyboard.pressed('w'):
+        time.sleep(1.9)
+
+def teleportBack(keyboard):
+    keyPress(keyboard, 'g')
+    with keyboard.pressed('s'):
+        time.sleep(0.05)
+    # with keyboard.pressed('a'):
+    #     time.sleep(0.05)
+    waitTime = 1.6
+    time.sleep(waitTime/2)
+    keyPress(keyboard, 'e')
+    time.sleep(waitTime/2)
+    keyPress(keyboard, 'e')
+    
+def useSkill(mouse):
+    keyPress(mouse, Button.middle)
+
+def keyPress(controller, key):
+    controller.press(key)
+    time.sleep(1)
+    controller.release(key)
+
+def runeHarvest(mouse, keyboard):
+    time.sleep(6)
+    initialPath(keyboard)
+    useSkill(mouse)
+    time.sleep(1)
+    teleportBack(keyboard)
+
+def on_press(key):
+    pass
+
+def on_release(key):
+    global active, endProgram
+    if key == Key.f10:
+        active = not active
+    if key == Key.f8:
+        endProgram = True       
+def main():
+    keyboard = Controller()
+    mouse = MouseController()
+    global active, endProgram 
+    active = False
+    endProgram = False
+
+    listener = pynput.keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+
+    listener.start()
+    while True:
+        if endProgram:
+            break
+        if active:
+            runeHarvest(mouse, keyboard)
+        time.sleep(1)
+
+    # for i in [0,1,2,3]:
+    #     runeHarvest(mouse, keyboard)
+
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,25 @@
+## Requirements:
+1. Sacred relic sword equipped in the right hand.
+2. Be able to one shot the enemies in the farming area with its golden wave ability.
+3. The map needs to be at the exact zoom level as when loging in the game. So, better 
+to log out and log back in to start the farming process if map zoom level was changed.
+4. Ability button set to middle mouse button. If you do not want to change your settings,
+you can modify the useSkill function in the script to match your mapping.
+
+## Optional equipment:
+1. Shard of Alexander: can help you achieve the one-shot enemies requirement.
+2. Golden scarab: increase your rune yield.
+
+## Farming procedure:
+1. Run the script.
+2. Login game.
+3. Open map and teleport to Palace Approach Ledge-Road site of grace in Mohgwyn Palace. (Do not change map zoom level.)
+4. Press F10.
+5. Enjoy!
+6. Press F10 again to pause farming. You may need to wait for it to finish one more cycle.
+7. Press F8 to terminate the program. 
+
+## Notes:
+1. It may break sometimes due to a walking enemy not being one-shotted for some unknown reasons when walked to a certain position.
+The cycle will continue but runes up until that point will drop. This rarely happens and did not happen at all when reached faith 80
+during a 6 hour test run.  
